7a4f69a0db8a5358408eb7f4e9058ea90db2f4c3
C. Titus Brown <titus@idyll.org>
2015-02-01 11:41:12 -0500

    initial read mapper

---
+++ locate-reads-in-ref.py
@@ -0,0 +1,103 @@
+#! /usr/bin/env python
+import argparse
+import khmer
+import screed
+import sys
+import graphAlignment
+
+REGIONSIZE=100
+
+
+def turn_locations_into_regions(refposns):
+    d = {}
+    for ref, pos in refposns:
+        x = d.get(ref, set())
+        x.add(pos)
+        d[ref] = x
+
+    regions = []
+    for k in d.keys():
+        posns = list(d[k])
+        posns.sort()
+
+        start = posns[0]
+        i = 0
+        while 1:
+            while i < len(posns) - 1 and \
+                  (posns[i+1] - posns[i]) < REGIONSIZE:
+                i += 1
+
+            # done with region - record.
+            end = posns[i]
+            regions.append((k, start, end))
+
+            # done over all? exit.
+            if i == len(posns) - 1:
+                break
+
+            # move on to next region.
+            i += 1
+            start = posns[i]
+
+    return regions
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('reference')
+    parser.add_argument('readfile')
+    args = parser.parse_args()
+
+    ct = khmer.new_counting_hash(21, 1e7, 4)
+
+    tags_to_positions = {}
+    references = {}
+    for record in screed.open(args.reference):
+        # store for later retrieval - in memory, for now.
+        references[record.name] = record.sequence
+
+        # load into graph & tag
+        ct.consume_and_tag(record.sequence)
+
+        # track positions in reference by tag
+        tagposns = ct.get_tags_and_positions(record.sequence)
+        for pos, tag in tagposns:
+            x = tags_to_positions.get(tag, [])
+            x.append((record.name, pos))
+            tags_to_positions[tag] = x
+
+    # now, walk through the reads and map to graph
+    aligner = khmer.new_readaligner(ct, 0, 1.0)
+    for read in screed.open(args.readfile):
+        readseq = read.sequence.replace('N', 'A')
+        score, g, r, truncated = aligner.align(readseq)
+        if truncated:
+            print >>sys.stderr, "IGNORING read", read.name
+            continue
+
+        refseq = g.replace('-', '')
+        ptags = ct.get_tags_and_positions(refseq)
+        assert len(ptags)
+
+        refposns = []
+        for pos, tag in ptags:
+            refposns.extend(tags_to_positions[tag])
+
+
+        regions = turn_locations_into_regions(refposns)
+        print read.name, 'found', len(regions)
+        for (ref, start, end) in regions:
+            referenceseq = references[ref]
+            start = max(start - REGIONSIZE/2, 0)
+            end = min(end + REGIONSIZE/2, len(referenceseq))
+            regionseq = referenceseq[start:end]
+            print len(regionseq), start, end
+
+            nct = khmer.new_counting_hash(21, 1e5, 4)
+            nct.consume(regionseq)
+            naligner = khmer.new_readaligner(nct, 1, 1.0)
+            score, galign = graphAlignment.align_long(nct, naligner, readseq)
+            print galign
+
+if __name__ == '__main__':
+    main()
