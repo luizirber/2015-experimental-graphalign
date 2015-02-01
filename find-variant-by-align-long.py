a16d1b5f632a355410f2c9938fffa163b8eb907a
C. Titus Brown <titus@idyll.org>
2015-01-31 22:14:27 -0500

    misc refactoring

--- find-variant-by-align-long.py
+++ find-variant-by-align-long.py
@@ -3,137 +3,9 @@ import sys
 import argparse
 import screed
 import khmer
-from graphAlignment import GraphAlignment, make_gap, reverse_complement
-
-REGIONSIZE = 50
-
-
-def stitch(galign, K):
-    """Stitch together multiple alignments, assuming all overlaps are
-    off-by-one on the right end."""
-    ga = []
-    ra = []
-
-    n = 0
-    for gg in galign[:-1]:
-        g = gg.g
-        r = gg.r
-        assert len(g) == len(r)
-        
-        ga.append(g[:-K + 1])
-        ra.append(r[:-K + 1])
-        n += 1
-
-    ga.append(galign[-1].g)
-    ra.append(galign[-1].r)
-
-    return GraphAlignment("".join(ga), "".join(ra))
-
-
-def align_segment_right(aligner, seq, next_ch=None):
-    assert len(seq) >= 21
-    score, g, r, truncated = aligner.align(seq)
-    galign = GraphAlignment(g, r)
-
-    # did it fail to align across entire segment?
-    if truncated:
-        aligned_length = galign.refseqlen()
-        unaligned_len = len(seq) - aligned_length
-
-        # if we are given next ch, try aligning backwards from next seed
-        if next_ch:
-            unaligned_seq = seq[-unaligned_len:]
-            sr, ggr = align_segment_left(aligner, unaligned_seq + next_ch)
-            galign += ggr[:-1]
-            score += sr                 # need to adjust score down... @@
-        else:
-            # if not, just build a gap...
-            galign += make_gap(seq[-unaligned_len:])
-
-    return score, galign
-
-
-def align_segment_left(aligner, seq):
-    seq_rc = reverse_complement(seq)
-    score, galign = align_segment_right(aligner, seq_rc)
-    return score, galign.rc()
-
-
-def align_long(ct, aligner, sta):
-    K = ct.ksize()
-    
-    # first, pick seeds for each chunk
-    seeds = []
-    for start in range(0, len(sta), REGIONSIZE):
-        region = sta[start:start + REGIONSIZE + K - 1]
-        if len(region) < K:
-            assert len(sta) - start < REGIONSIZE
-            break
-        seed_pos = find_highest_abund_kmer(ct, region)
-        seeds.append(start + seed_pos)
-
-    print >>sys.stderr, 'picked %d seeds' % len(seeds)
-    assert len(seeds) == len(set(seeds))
-
-    # then, break between-seed intervals down into regions, starting at
-    # first seed.
-    region_coords = []
-    for i in range(len(seeds) - 1):
-        seed_pos = seeds[i]
-        end_seed = seeds[i + 1] - 1
-        region_coords.append((seed_pos, end_seed + K))
-
-    if len(sta) - seeds[-1] > K:
-        region_coords.append((seeds[-1], len(sta)))
-
-    # start building piecewise alignments
-    alignments = []
-    scores = []
-
-    n = 0
-    for (start, end) in region_coords[:-1]:
-        #print 'aligning', n, start, end, (end - start - K)
-        score, galign = align_segment_right(aligner, sta[start:end],
-                                          next_ch=sta[end])
-        
-        scores.append(score)
-        alignments.append(galign)
-        
-        n += 1
-
-    # deal with end:
-    (start, end) = region_coords[-1]
-    score, galign = align_segment_right(aligner, sta[start:end])
-    
-    alignments.append(galign)
-    scores.append(score)
-    
-    # deal with beginning, too: reverse align from first seed.
-    leftend = sta[0:seeds[0] + K]
-    score, galign = align_segment_left(aligner, leftend)
-
-    galign = galign[:-1]                # trim off seed k-mer
-    alignments.insert(0, galign)
-    scores.insert(0, score)
-
-    # stitch all the alignments together
-    final = stitch(alignments, K)
-
-    return sum(scores), final
-
-
-def find_highest_abund_kmer(ct, r):
-
-    pos = 0
-    abund = ct.get(r[:ct.ksize()])
-    for kstart in range(1, len(r) - ct.ksize() + 1):
-        kmer = r[kstart:kstart + ct.ksize()]
-        count = ct.get(kmer)
-
-        if count > abund:
-            pos = kstart
-
-    return pos
+from graphAlignment import align_long, GraphAlignment, make_gap, \
+     reverse_complement, \
+     AlignmentIndex
 
 
 def main():
@@ -146,10 +18,10 @@ def main():
     aligner = khmer.new_readaligner(ct, 5, 1.0)
 
     for record in screed.open(args.ref):
-        s = record.sequence
-        s = s.replace('N', 'A')
+        seq = record.sequence
+        seq = seq.replace('N', 'A')
 
-        score, alignment = align_long(ct, aligner, s)
+        score, alignment = align_long(ct, aligner, seq)
 
         g = alignment.g
         r = alignment.r
@@ -159,15 +31,19 @@ def main():
         for start in range(0, len(alignment), 60):
             print alignment[start:start+60]
 
-        print len(s), alignment.refseqlen()
+        if 1:
+            print len(seq), alignment.refseqlen()
 
-        for i in range(0, len(s)):
-            break                       # @CTB debugging code
-            a = alignment[:i]
-            pos = a.refseqlen()
-            if alignment[i][1] in 'ACGT':
-                print i, alignment[i], pos, s[pos], alignment[i][1] == s[pos]
-                assert alignment[i][1] == s[pos]
+            gidx = AlignmentIndex(alignment)
+
+            for i in range(len(alignment)):
+                if alignment[i][1] in 'ACGT':
+                    assert alignment[i][1] == seq[gidx.get_ri(i)]
+
+            for j in range(len(seq)):
+                gi = gidx.get_gi(j)
+                if alignment[gi][1] in 'ACGT' or 1:
+                    assert seq[j] == alignment[gi][1]
 
 
 if __name__ == '__main__':
--- graphAlignment.py
+++ graphAlignment.py
@@ -1,3 +1,4 @@
+import sys
 import string
 import array
 __complementTranslation = string.maketrans('ACTGactg-=Nn', 'TGACtgac-=Nn')
@@ -50,7 +51,7 @@ class GraphAlignment(object):
 
     def refseqlen(self):
         return self.r.count('G') + self.r.count('C') + self.r.count('T') + \
-               self.r.count('A') + self.r.count('N')
+               self.r.count('A')
 
     def __getitem__(self, i):
         if isinstance(i, slice):
@@ -88,3 +89,197 @@ class GraphAlignment(object):
 
 def make_gap(r):
     return GraphAlignment('='*len(r), r)
+
+
+def _index_alignment(galign, freq=100):
+    g_to_r = {}
+    r_to_g = {}
+
+    si = 0
+    for gi, (a, b) in enumerate(zip(galign.g, galign.r)):
+        if gi % freq == 0:
+            g_to_r[gi] = si
+        if si % freq == 0:
+            r_to_g[si] = gi
+
+        if b in 'ACGT':
+            si += 1
+
+    return g_to_r, r_to_g
+
+
+class AlignmentIndex(object):
+    def __init__(self, galign, freq=100):
+        self.g_to_r, self.r_to_g = _index_alignment(galign, int(freq))
+        self.galign = galign
+        self.freq = int(freq)
+
+    def get_gi(self, ri):
+        rpost = int(ri / self.freq) * self.freq
+        gpost = self.r_to_g[rpost]
+
+        diff = ri - rpost
+        gi = gpost
+        
+        while diff > 0:
+            (a, b) = self.galign[gi]
+            if b in 'ACGT':
+                diff -= 1
+            gi += 1
+
+        # make sure it's on a valid letter ;)
+        while 1:
+            (a, b) = self.galign[gi]
+            if b in 'ACGT':
+                break
+            gi += 1
+            
+        return gi
+
+    def get_ri(self, gi):
+        gpost = int(gi / self.freq) * self.freq
+        ri = self.g_to_r[gpost]
+
+        diff = gi - gpost
+        while diff > 0:
+            (a, b) = self.galign[gpost]
+            diff -= 1
+            gpost += 1
+            if b in 'ACGT':
+                ri += 1
+
+        return ri
+
+
+REGIONSIZE = 50
+
+
+def stitch(galign, K):
+    """Stitch together multiple alignments, assuming all overlaps are
+    off-by-one on the right end."""
+    ga = []
+    ra = []
+
+    len_so_far = 0
+
+    n = 0
+    for gg in galign[:-1]:
+        g = gg.g
+        r = gg.r
+        assert len(g) == len(r)
+        
+        ga.append(g[:-K + 1])
+        ra.append(r[:-K + 1])
+
+        n += 1
+
+    ga.append(galign[-1].g)
+    ra.append(galign[-1].r)
+
+    return GraphAlignment("".join(ga), "".join(ra))
+
+
+def align_segment_right(aligner, seq, next_ch=None):
+    assert len(seq) >= 21
+    score, g, r, truncated = aligner.align(seq)
+    galign = GraphAlignment(g, r)
+
+    # did it fail to align across entire segment?
+    if truncated:
+        aligned_length = galign.refseqlen()
+        unaligned_len = len(seq) - aligned_length
+
+        # if we are given next ch, try aligning backwards from next seed
+        if next_ch:
+            unaligned_seq = seq[-unaligned_len:]
+            sr, ggr = align_segment_left(aligner, unaligned_seq + next_ch)
+            galign += ggr[:-1]
+            score += sr                 # need to adjust score down... @@
+        else:
+            # if not, just build a gap...
+            galign += make_gap(seq[-unaligned_len:])
+
+    return score, galign
+
+
+def align_segment_left(aligner, seq):
+    seq_rc = reverse_complement(seq)
+    score, galign = align_segment_right(aligner, seq_rc)
+    return score, galign.rc()
+
+
+def align_long(ct, aligner, sta):
+    K = ct.ksize()
+    
+    # first, pick seeds for each chunk
+    seeds = []
+    for start in range(0, len(sta), REGIONSIZE):
+        region = sta[start:start + REGIONSIZE + K - 1]
+        if len(region) < K:
+            assert len(sta) - start < REGIONSIZE
+            break
+        seed_pos = find_highest_abund_kmer(ct, region)
+        seeds.append(start + seed_pos)
+
+    print >>sys.stderr, 'picked %d seeds' % len(seeds)
+    assert len(seeds) == len(set(seeds))
+
+    # then, break between-seed intervals down into regions, starting at
+    # first seed.
+    region_coords = []
+    for i in range(len(seeds) - 1):
+        seed_pos = seeds[i]
+        end_seed = seeds[i + 1] - 1
+        region_coords.append((seed_pos, end_seed + K))
+
+    if len(sta) - seeds[-1] > K:
+        region_coords.append((seeds[-1], len(sta)))
+
+    # start building piecewise alignments, anchored by seeds.
+    alignments = []
+    scores = []
+
+    n = 0
+    for (start, end) in region_coords[:-1]:
+        score, galign = align_segment_right(aligner, sta[start:end],
+                                            next_ch=sta[end])
+        
+        
+        scores.append(score)
+        alignments.append(galign)
+        
+        n += 1
+
+    # deal with end (no possibility of alignment from right)
+    (start, end) = region_coords[-1]
+    score, galign = align_segment_right(aligner, sta[start:end])
+    
+    alignments.append(galign)
+    scores.append(score)
+    
+    # deal with beginning, too: reverse align from first seed.
+    leftend = sta[0:seeds[0] + K]
+    score, galign = align_segment_left(aligner, leftend)
+
+    galign = galign[:-1]                # trim off seed k-mer
+    alignments.insert(0, galign)
+    scores.insert(0, score)
+
+    # stitch all the alignments together
+    final = stitch(alignments, K)
+
+    return sum(scores), final
+
+
+def find_highest_abund_kmer(ct, r):
+
+    pos = 0
+    abund = ct.get(r[:ct.ksize()])
+    for kstart in range(1, len(r) - ct.ksize() + 1):
+        kmer = r[kstart:kstart + ct.ksize()]
+        count = ct.get(kmer)
+
+        if count > abund:
+            pos = kstart
+
+    return pos
